5622633682243506e670cdd0f5a3332b314946c6
erica2605@live.com <erica2605@live.com>
2020-04-06 11:53:11 -0700

    added initial scoreboard

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import GridBoard from './components/grid-board'
 import NextBlock from './components/next-block'
+import ScoreBoard from './components/score-board'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       </header>
       <GridBoard />
       <NextBlock />
+      <ScoreBoard />
     </div>
   );
 }
