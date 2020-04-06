9e7cccec83f0a590b75058a95c351b05d4595109
erica2605@live.com <erica2605@live.com>
2020-04-06 12:05:39 -0700

    added inital message popup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GridBoard from './components/grid-board'
 import NextBlock from './components/next-block'
 import ScoreBoard from './components/score-board'
 import Controls from './components/controls'
+import MessagePopup from './components/message-popup'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <NextBlock />
       <ScoreBoard />
       <Controls />
+      <MessagePopup />
     </div>
   );
 }
